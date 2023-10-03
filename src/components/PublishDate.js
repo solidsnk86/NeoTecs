e68ab1a4bfe538b2c887434caa81a3ac94bb1392
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-03 03:21:52 -0300

    remove some sass class

--- src/components/PublishDate.js
+++ src/components/PublishDate.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Clock } from 'lucide-react';
+import { Calendar } from 'lucide-react';
 
 export const PublishDate = () => {
   const date = '1 de Octubre';
   return (
     <div>
-      <p className="text-gray-700">
-        <Clock className="inline-flex w-4 h-4 mr-2 mb-1" />
+      <p className="text-gray-700 text-sm italic">
+        <Calendar className="inline-flex w-[13px] h-[13px] mr-2 mb-1" />
         Publicado el
         <span className="inline-flex mx-1">{date}</span>
       </p>
