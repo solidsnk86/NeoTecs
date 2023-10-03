5b2c3d0bbf2f3edcf1726a1777fd795bac5bceb1
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-03 03:52:02 -0300

    update

--- src/components/PublishDate.js
+++ src/components/PublishDate.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Calendar } from 'lucide-react';
 
 export const PublishDate = () => {
-  const currentDate = new Date();
+  const postedtDate = '01/10/2023';
 
   return (
     <div>
       {[
         {
           text: 'Publicado el ',
-          date: currentDate,
+          date: postedtDate,
           icon: (
             <Calendar className="inline-flex w-[13px] h-[13px] mr-2 mb-1" />
           ),
@@ -17,9 +17,7 @@ export const PublishDate = () => {
       ].map((item) => (
         <p className="text-gray-700 text-sm italic">
           {item.text}
-          <span className="inline-flex mx-1">
-            {item.date.toLocaleDateString()}
-          </span>
+          <span className="inline-flex mx-1">{item.date}</span>
           {item.icon}
         </p>
       ))}
