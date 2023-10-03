dab024fba9cb0f2077cde705e893ad0011b0cc38
Gabriel Calcagni <93176365+solidsnk86@users.noreply.github.com>
2023-10-03 00:59:34 -0300

    Update PublishDate.js

--- src/components/PublishDate.js
+++ src/components/PublishDate.js
@@ -5,7 +5,7 @@ export const PublishDate = () => {
   const date = '1 de Octubre';
   return (
     <div>
-      <p className="text-gray-700">
+      <p className="text-gray-700 text-sm">
         <Clock className="inline-flex w-4 h-4 mr-2 mb-1" />
         Publicado el
         <span className="inline-flex mx-1">{date}</span>
