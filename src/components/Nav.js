0b34b350f500e78c0b635d6807d36884105be257
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-03 22:36:40 -0300

    updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
-import { Github } from 'lucide-react';
+import { Github, SunMoon } from 'lucide-react';
 
 export const Nav = ({ className }) => {
+  const [isDarkMode, setIsLightMode] = useState(false);
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    if (savedMode) {
+      setIsLightMode(savedMode === 'true');
+    } else {
+      setIsLightMode(
+        window.matchMedia('(prefers-color-scheme: light)').matches,
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    document.body.classList.toggle('light', isDarkMode);
+
+    localStorage.setItem('darkMode', isDarkMode.toString());
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsLightMode((prevMode) => !prevMode);
+  };
+
   return (
     <div className={className || ''}>
       <div className="h-16 flex justify-between max-w-screen-xl mx-auto px-4 md:px-8 z-10 relative text-white">
@@ -15,15 +39,23 @@ export const Nav = ({ className }) => {
             />
           </Link>
         </div>
-        <Link
-          href="https://github.com/solidsnk86"
-          className="flex items-center space-x-4 mb-3"
-        >
-          <span className=" px-2 py-1 rounded-full bg-slate-200 font-semibold text-sm text-[#222] hover:opacity-[.6] hover:transition-all">
-            Sígueme
-          </span>
-          <Github className="cursor-pointer hover:opacity-[.7] w-7 h-7" />
-        </Link>
+        <aside className="inline-flex space-x-3">
+          <Link
+            href="https://github.com/solidsnk86"
+            className="flex items-center space-x-2 mb-3 hover:opacity-[.6] hover:transition-all"
+          >
+            <span className=" px-2 py-1 rounded-full bg-slate-200 font-semibold text-sm text-[#222]">
+              Sígueme
+            </span>
+            <Github className="cursor-pointer w-7 h-7" />
+          </Link>
+          <SunMoon
+            className="inline-flex w-8 h-8 mt-[10px] hover:opacity-[.6] cursor-pointer"
+            onClick={toggleDarkMode}
+          >
+            {isDarkMode ? 'Modo Claro' : 'Modo Oscuro'}
+          </SunMoon>
+        </aside>
       </div>
     </div>
   );
