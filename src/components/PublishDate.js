d885993b4b1b43703dfbe29aa0daf07f97db04fb
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-08 07:39:03 -0300

    updates stuffs

--- src/components/PublishDate.js
+++ src/components/PublishDate.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
 import { History } from 'lucide-react';
 
 export const PublishDate = () => {
   const postedtDate = '01 Octubre, 2023';
 
+  const BackHome = [{ link: 'Inicio /', href: '/' }];
+
   return (
-    <div>
+    <div className="flex space-x-2">
+      {BackHome.map((i) => (
+        <Link key={i.link} href={i.href}>
+          <p className="text-gray-700 text-sm italic no-underline hover:animate-pulse antialiased">
+            {i.link}
+          </p>
+        </Link>
+      ))}
       {[
         {
-          text: 'Publicado ',
+          text: 'Publicado, ',
           date: postedtDate,
-          icon: <History className="inline-flex w-[13px] h-[13px] mr-2 mb-1" />,
+          icon: (
+            <History className="inline-flex w-[13px] h-[13px] mr-2 mb-[2px]" />
+          ),
         },
       ].map((item) => (
         <p className="text-gray-700 text-sm italic">
