c8b86e3aa24d02f2f0cc019c0a254bc94616840a
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-09 19:23:09 -0300

    ..

--- src/components/ImagesBox.js
+++ src/components/ImagesBox.js
@@ -1,30 +1,30 @@
 import { Atropos } from 'atropos/react';
 
-export const MovieBox = ({ bg, logo1, logo2, logo3 }) => {
+export const ImgBox = ({ bg, logo1, logo2, logo3 }) => {
   return (
     <div className="w-64 m-6">
       <Atropos className="movie-box" rotateTouch="scroll-y">
         <div className="movie-box-bg" data-atropos-offset="-5">
-          <img src={`/images/demos/movies/${bg}`} alt="movie bg" />
+          <img src={`/images/GerArt/${bg}`} alt="movie bg" />
         </div>
 
         <img
           className="movie-box-logo"
           data-atropos-offset="0"
-          src={`/images/demos/movies/${logo1}`}
+          src={`/images/GerArt/${logo1}`}
           alt="movie logo 1"
         />
         <img
           className="movie-box-studio"
           data-atropos-offset="2.5"
-          src={`/images/demos/movies/${logo2}`}
+          src={`/images/GerArt/${logo2}`}
           alt="movie logo 2"
         />
         {logo3 && (
           <img
             className="movie-box-studio"
             data-atropos-offset="5"
-            src={`/images/demos/movies/${logo3}`}
+            src={`/images/GerArt/${logo3}`}
             alt="movie logo 3"
           />
         )}
