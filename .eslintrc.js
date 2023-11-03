d965e838a3b057505c378cc69bca3dbcfdb7be79
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-11-03 18:35:29 -0300

    plugin export

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
