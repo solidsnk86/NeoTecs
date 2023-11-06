083d48f7b15d829bfe5cec22fc418dbf180d987b
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-11-06 06:58:58 -0300

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -99,12 +99,12 @@ export const Nav = ({ className }) => {
                 <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z" />
               </svg>
             </div>
-            <ul className="absolute right-0 top-full hidden space-y-1 whitespace-nowrap rounded-xl bg-surface-3 px-3 py-4 text-sm group-hover:block">
+            <ul className="absolute right-2 top-6 hidden space-y-1 whitespace-nowrap rounded-xl bg-[#0F0F0F] px-3 py-4 text-sm group-hover:block">
               <li>
                 <button
                   type="button"
                   onClick={() => setSetting('light')}
-                  className="flex w-full items-center space-x-3 rounded-md px-3 py-1 font-medium leading-6 text-primary hover:bg-primary hover:text-on-primary hover:no-underline"
+                  className="flex w-full items-center space-x-3 rounded-md px-3 py-1 font-medium leading-6 text-primary hover:bg-gray-800 hover:text-on-primary hover:no-underline"
                 >
                   <svg
                     className="h-6 w-6"
@@ -123,7 +123,7 @@ export const Nav = ({ className }) => {
                 <button
                   type="button"
                   onClick={() => setSetting('dark')}
-                  className="flex w-full items-center space-x-3 rounded-md px-3 py-1 font-medium leading-6 text-primary hover:bg-primary hover:text-on-primary hover:no-underline"
+                  className="flex w-full items-center space-x-3 rounded-md px-3 py-1 font-medium leading-6 text-primary hover:bg-gray-800 hover:text-on-primary hover:no-underline"
                 >
                   <svg
                     className="h-6 w-6"
@@ -142,7 +142,7 @@ export const Nav = ({ className }) => {
                 <button
                   type="button"
                   onClick={() => setSetting('system')}
-                  className="flex w-full items-center space-x-3 rounded-md px-3 py-1 font-medium leading-6 text-primary hover:bg-primary hover:text-on-primary hover:no-underline"
+                  className="flex w-full items-center space-x-3 rounded-md px-3 py-1 font-medium leading-6 text-primary hover:bg-gray-800 hover:text-on-primary hover:no-underline"
                 >
                   <svg
                     className="h-6 w-6"
