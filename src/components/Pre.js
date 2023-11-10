ef69ce8d91ec9314214b63a4bb86338d05f65d57
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-11-10 02:41:56 -0300

    update toast prefers-color-scheme dark y light

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,6 @@
+import React, { useRef } from 'react';
 import { Highlight, themes } from 'prism-react-renderer';
 import stripIndent from 'strip-indent';
-import React, { useRef } from 'react';
 import { Copy } from 'lucide-react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -19,11 +19,17 @@ export const Pre = ({ children, lang = '' }) => {
       window.getSelection().removeAllRanges();
       window.getSelection().addRange(range);
     }
+
     try {
       document.execCommand('copy');
+
+      const isDarkMode = window.matchMedia(
+        '(prefers-color-scheme: dark)',
+      ).matches;
+
       toast.success('Contenido copiado al portapapeles', {
         position: toast.POSITION.BOTTOM_LEFT,
-        theme: 'dark',
+        theme: isDarkMode ? 'dark' : 'light',
       });
     } catch (err) {
       toast.error('Error al intentar copiar al portapapeles', {
@@ -41,7 +47,7 @@ export const Pre = ({ children, lang = '' }) => {
 
       <Copy
         onClick={handleCopyClick}
-        className=" w-5 h-5 inline-flex my-auto absolute z-10 right-2 mt-3 cursor-pointer rounded hover:text-white transition-all"
+        className="w-5 h-5 inline-flex my-auto absolute z-10 right-2 mt-3 cursor-pointer rounded text-text-strong hover:brightness-150 transition-all"
       />
       <Highlight
         theme={themes.vsDark}
@@ -51,9 +57,9 @@ export const Pre = ({ children, lang = '' }) => {
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre className={className} style={style} ref={preRef}>
             {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
+              <div {...getLineProps({ line, key: i })} key={i}>
                 {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
+                  <span {...getTokenProps({ token, key })} key={key} />
                 ))}
               </div>
             ))}
