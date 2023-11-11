c6d11c458f7bde14e4dabaaab5a15d0f44c3a91b
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-11-10 22:58:53 -0300

    update

--- djangoLib/djangoLib/settings.py
+++ djangoLib/djangoLib/settings.py
@@ -19,9 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-x-t^l27#36yi*ylgk+0#c9q(gv&x(g90s)j_luz_@(_4#ctrda'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
