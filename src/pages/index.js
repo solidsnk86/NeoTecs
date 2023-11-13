02e04e1ddfb478fe54ea385cd665c00e714f3130
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-11-13 19:24:32 -0300

    ura

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import { YouTubeVideoSection } from '../components/YouTubeVideoSection';
 import { MarqueeLogos } from '../sections/Brands';
 import { ShimmerButton } from '../components/magicui/ShimmerButton';
 import { Card } from '../components/Card';
-import { MT } from '../components/MeteorLanguages';
 
 const HomeBlock = ({ className = '', children }) => {
   return (
@@ -53,7 +52,6 @@ export default function Home() {
   return (
     <div className="text-text-primary">
       <Nav />
-      <MT />
       <HomeHeader />
       <HomeBlock>
         <HomeBlockTitle>Configurá tu CPE</HomeBlockTitle>
