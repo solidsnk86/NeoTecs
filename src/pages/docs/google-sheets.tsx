9d6cfbd4aed62c1238de766ce0b8897a2831655a
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-12-19 18:44:56 -0300

    update google-sheets base

--- src/pages/docs/google-sheets.tsx
+++ src/pages/docs/google-sheets.tsx
@@ -1,26 +1,42 @@
-import { Card } from "../../components/Card"
+import { useState, useEffect } from 'react';
 
-export default async function CsvSheets() {
-    const csv = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTJzdlPCNykOkJWbmkFcc3Iw0ZNSVwEnUDgrtCGr0pfDRfVNZmp_iTMaGZfU0z4njNWlFoJ3y3LwFPv/pub?output=csv')
-        .then((res) => res.text())
-    const drawings = csv
-        .split("\n")
-        .slice(1)
-        .map((row) => {
-            const [id, name, image, price] = row.split(",")
+export default function CsvSheets() {
+    const [drawings, setDrawings] = useState([]);
 
-            return { id, name, image, price: Number(price) }
-        })
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    'https://docs.google.com/spreadsheets/d/e/2PACX-1vTJzdlPCNykOkJWbmkFcc3Iw0ZNSVwEnUDgrtCGr0pfDRfVNZmp_iTMaGZfU0z4njNWlFoJ3y3LwFPv/pub?output=csv'
+                );
+                const csv = await response.text();
+                const parsedDrawings = csv
+                    .split('\n')
+                    .slice(1)
+                    .map((row) => {
+                        const [id, name, description, image, price] = row.split(',');
+                        return { id, name, description, image, price: Number(price) };
+                    });
+                setDrawings(parsedDrawings);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <section>
+        <section className='w-1/2 h-screen justify-center mx-auto text-text-primary p-10 space-y-5'>
             {drawings.map((pic) => (
-                <Card>
-                    {pic.id}
-                    {pic.image}
-                    {pic.name}
-                    {pic.price}
-                </Card>
+                <article key={pic.id}>
+                    <p className='text-2xl font-mono text-center py-2'>{pic.id}</p>
+                    <img className='rounded-md' src={pic.image} alt='Drawing GerArt' />
+                    <p>{pic.name}</p>
+                    <p>{pic.description}</p>
+                    <span className='bg-red-400/60 p-1 rounded w-fit float-right'>Price: u$d{pic.price}</span>
+                </article>
             ))}
         </section>
-    )
+    );
 }
