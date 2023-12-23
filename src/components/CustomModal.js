915424373bba64eb539417c48f7b1f0722f10f67
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-12-23 05:54:31 -0300

    update click on image open modal

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
-const CustomModal = ({ isOpen, onClose, image }) => {
+const CustomModal = ({ isOpen, onClose, imageURL }) => {
   return (
     <Modal
       isOpen={isOpen}
@@ -12,12 +12,12 @@ const CustomModal = ({ isOpen, onClose, image }) => {
       contentLabel="Ampliar Imagen"
     >
       <img
-        src={image}
-        className="w-full h-full xl:h-[200%]"
+        src={imageURL}
+        className="w-full h-full xl:h-[200%] xl:w-[200%]"
         alt="Ampliación de Imagen"
       />
       <button
-        className="my-3 bg-button-variant text-text-variant p-2 rounded-full"
+        className="my-3 bg-button-variant text-text-variant p-2 rounded-full font-semibold hover:opacity-90"
         onClick={onClose}
       >
         Cerrar
