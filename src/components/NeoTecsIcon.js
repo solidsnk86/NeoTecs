cbf5900678cdb42a27715705b8c5c71404dcfeba
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-01-03 20:49:10 -0300

    atributtes

--- src/components/NeoTecsIcon.js
+++ src/components/NeoTecsIcon.js
@@ -8,6 +8,7 @@ export const NeoTecsIcon = (props) => {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...attrs}
     >
       <g clip-path="url(#clip0_1_3)">
         <rect width="24" height="24" fill="transparent" />
