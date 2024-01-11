7983a48a59b1ad0ea5626e81f52b6a7ea3fc950b
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-01-11 08:59:25 -0300

    update

--- src/pages/docs/markdown-render.js
+++ src/pages/docs/markdown-render.js
@@ -10,7 +10,6 @@ import Link from 'next/link';
 import { OpenInNew } from '@mui/icons-material';
 import HeaderTitle from '../../components/HeaderTitlte';
 import IndexTitle from '../../components/IndexTitle';
-import CopyToClipboard from '../../components/CopyToClipboard';
 export default function MarkDownRender() {
   return (
     <TitlesContextProvider>
@@ -287,7 +286,6 @@ export default function MarkDownRender() {
             de NeoTecs.
           </p>
           <ShareButton setTitle={MarkDownRender.title} />
-          <CopyToClipboard />
         </div>
       </div>
       <Footer />
