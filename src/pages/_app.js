49f5ee51acc9c5bc80190706d6f6f6e8e53ff152
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-01-13 00:46:39 -0300

    gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ import { DoNotCopy } from '../components/DoNotCopy';
 import { NeoTecsIcon } from '../components/NeoTecsIcon';
 
 function MyApp({ Component, pageProps, router }) {
-  const title = Component.title || 'Neotecs - Tutoriales';
+  const title = Component.title || 'Neotecs - Informática';
   const description =
     'Aprende programación en este curso gratuito de NeoTecs, que abarca desde conceptos básicos hasta niveles avanzados. Además, disponemos de documentación para facilitar la configuración rápida y sencilla de tu WiFi.';
 
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps, router }) {
         <meta property="og:description" content={description} />
         <meta
           property="og:image"
-          content="/images/NeoTecs _Tutorial_logo.png"
+          content="/images/logos/NeoTecs _Tutorial_logo.png"
         />
         <meta property="og:site_name" content="Neotecs" />
         <meta name="twitter:card" content="summary_large_image" />
--- src/shared/use-ga.js
+++ src/shared/use-ga.js
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 export const useGA = (router) => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-HL41YE945C';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-HLQD7TD09P';
     script.onload = () => {
       window.dataLayer = window.dataLayer || [];
       window.gtag = function () {
         window.dataLayer.push(arguments);
       };
       window.gtag('js', new Date());
-      window.gtag('config', 'G-HL41YE945C');
+      window.gtag('config', 'G-HLQD7TD09P');
     };
     document.body.appendChild(script);
 
