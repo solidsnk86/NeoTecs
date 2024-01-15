7f1278251f5c8f920e8d15ff77d4579b48f05a03
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-01-15 13:32:33 -0300

    update

--- src/components/FeedDataFecth.tsx
+++ src/components/FeedDataFecth.tsx
@@ -39,24 +39,49 @@ export const FeedbackData = () => {
         window.open(emailLink);
     };
 
+    const handleDelete = async (id) => {
+        console.log('Eliminando comentario con id:', id);
+        try {
+            const { data, error } = await supabase
+                .from('feedback')
+                .delete()
+                .match({ id });
+
+            if (error) {
+                throw error;
+            }
+
+            console.log('Comentario eliminado con éxito.');
+
+            // Actualizar la lista de comentarios después de la eliminación
+            const { data: newData, error: newError } = await supabase
+                .from('feedback')
+                .select('*')
+                .order('fecha', { ascending: false });
+
+            if (newError) {
+                throw newError;
+            }
+
+            setItems(newData);
+        } catch (error) {
+            console.error('Error deleting feedback:', error.message);
+        }
+    };
+
     return (
         <>
             {items.map((item) => (
-                <div className="bg-gray-100 dark:bg-zinc-800/40 border dark:border-zinc-800 p-4 rounded-xl my-10 mx-auto xl:w-1/2">
-                    <div
-                        key={item.id}
-                        className="text-text-primary text-xs xl:text-sm mb-4"
-                    >
+                <div className="bg-gray-100 dark:bg-zinc-800/40 border dark:border-zinc-800 p-4 rounded-xl my-10 mx-auto xl:w-1/2" key={item.id}>
+                    <div className="text-text-primary text-xs xl:text-sm mb-4">
                         <p className="text-right">{formatDate(item.fecha)}</p>
                         <h6>Comentario:</h6>
                         <p>{item.comentario}</p>
                         <p className="text-right">{item.nombre}</p>
-                        <p
-                            className="underline cursor-pointer w-fit"
-                            onClick={() => sendMail(item)}
-                        >
-                            Responder
-                        </p>
+                        <p className="underline cursor-pointer w-fit" onClick={() => sendMail(item)}>Responder</p>
+                        <button onClick={() => handleDelete(item.id)} className="text-red-500 underline cursor-pointer w-fit mt-2">
+                            Eliminar Comentario
+                        </button>
                     </div>
                 </div>
             ))}
