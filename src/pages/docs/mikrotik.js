6a6257550584a996fbf44e1fb451338ce8fe6cb5
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-01-21 16:18:48 -0300

    upd

--- src/pages/docs/mikrotik.js
+++ src/pages/docs/mikrotik.js
@@ -18,7 +18,6 @@ export default function MikrotikDocs() {
         <div className="w-full max-w-none prose px-4 md:px-8">
           <BrandsSwitch inline />
           <SectionTitle title="Configuración Mikrotik" />
-
           <section className="text-text-primary">
             <p>
               Este artículo aplica a los siguientes modelos:
@@ -32,23 +31,18 @@ export default function MikrotikDocs() {
               ingresar desde una pc, o desde un smartphone, a continuación voy a
               detallar los diferentes métodos para configurar la CPE.
             </p>
-
             <p>Configuración desde la PC:</p>
-
             <p>
               Desde una PC con Sistema Operativo de Windows, nos dirigimos a
               panel de control, para acceder a él, presionamos el botón
               <FaWindows className="inline-flex mx-1 text-xl text-[cornflowerblue]" />
               + R y escribimos:
             </p>
-
             <Pre lang="bash">{`ncpa.cpl`}</Pre>
-
             <p>
               <FaSignInAlt className="inline-flex mr-2" />
               enter, veremos la siguiente ventana:
             </p>
-
             <div className="images-client">
               <img
                 src="https://github.com/solidsnk86/neotecs.tech/blob/master/img/imagen-ap-router-propiedades-red.png?raw=true"
@@ -56,20 +50,17 @@ export default function MikrotikDocs() {
                 id="imgAP"
               />
             </div>
-
             <p>Vamos a "Propiedades"</p>
             <p>
               Luego seleccionamos protocolo TCP /IP y entramos en las
               propiedades del protocolo; colocamos lo siguiente:
             </p>
-
             <ol>
               <li>Dirección IP:</li>
               <Pre lang="bash">{`192.168.88.10`}</Pre>
               <li>Máscara de sub red:</li>
               <Pre lang="bash">{`255.255.255.0`}</Pre>
             </ol>
-
             <div className="images-client flex justify-center m-auto">
               <img
                 src="https://github.com/solidsnk86/neotecs.tech/blob/master/img/propiedades-ethernet-mikrotik.png?raw=true"
@@ -77,16 +68,12 @@ export default function MikrotikDocs() {
                 id="imgAP"
               />
             </div>
-
             <SectionTitle title="URL Mikrotik" />
             <p>Nos dirijimos al navegador y colocamos la url:</p>
-
             <Pre lang="bash">{`http://198.168.88.1`}</Pre>
-
             <p>
               Nos aparecera una interfaz para el login de la antena algo así:
             </p>
-
             <div className="images-client flex justify-center m-auto">
               <img
                 src="https://www.hardreset.info/es/lpb/mikrotik-login-page/mikrotik-routerboard-hex-s-rb760igs.jpeg"
@@ -94,13 +81,11 @@ export default function MikrotikDocs() {
                 id="imgAP"
               />
             </div>
-
             <p>
               A continuación, en usuario, ponemos (admin) y dejamos en blanco la
               contraseña. hacemos click en Login y veremos una interfaz
               principal así:
             </p>
-
             <div className="images-client flex justify-center m-auto">
               <img
                 src="https://www.technotrade.com.ua/userfiles/images/mikrotik_quickset_2012-10-12/quickset-menu.jpg"
@@ -108,16 +93,13 @@ export default function MikrotikDocs() {
                 id="imgAP"
               />
             </div>
-
             <SectionTitle title="Sección Quick Set" />
-
             <p>
               Seleccionamos el botón
               <span className="bg-[#CCCCCC] text-black border-[#1C67B8] border-[1px] mx-1 px-2 py-1">
                 Quick Set
               </span>
             </p>
-
             <div className="images-client flex justify-center m-auto">
               <img
                 src="https://wiki.mikrotik.com/images/thumb/2/24/Quickset639.png/717px-Quickset639.png?20170728075938"
@@ -125,13 +107,11 @@ export default function MikrotikDocs() {
                 id="imgAP"
               />
             </div>
-
             <p>
               Seleccionamos Modo (Router), Address Aquisition: (Automatic),
               tildamos las casillas DHCP server y NAT. Aplicamos la y guardamos
               la configuración.
             </p>
-
             <p>
               De ésta misma ventana veremos en la parte izquierda una ventana de
               Wireless Client, a veces por defecto tenemos que apretar el botón
@@ -140,10 +120,7 @@ export default function MikrotikDocs() {
               mejor señal en Dbi, por ejemplo un rango (-75Dbi a -50Dbi), es un
               rango óptimo de conexión; aplicamos los cambios.
             </p>
-
-            <ShareButton setTitle={MikrotikDocs.title} />
             <SectionTitle title="Sección WebFig" />
-
             <p>
               Luego de aplicar nos ubicamos en el WebFig dentro de esta solapa
               nos figurara a la izquierda el listado con todos los parámetros de
@@ -154,7 +131,6 @@ export default function MikrotikDocs() {
               red.- Una vez identificado el modelo ingresaremos a la placa,
               haciendo clik en el nombre de la misma en este caso es la Wan 2.-
             </p>
-
             <div className="images-client flex justify-center m-auto">
               <img
                 src="https://github.com/solidsnk86/neotecs.tech/blob/master/img/Web-Fig-Mikrotik.png?raw=true"
@@ -162,7 +138,6 @@ export default function MikrotikDocs() {
                 id="imgAP"
               />
             </div>
-
             <p>
               Una vez dentro de la placa de red de la antena veremos el
               siguiente solapa, es importante que vean los títulos siempre a la
@@ -175,7 +150,6 @@ export default function MikrotikDocs() {
               <li>Country: Argentina.</li>
               <li>Wireless Protocol: 802.11</li>
             </ul>
-
             <ol>
               <p className="bg-gray-800 w-fit px-2 text-[aliceblue] rounded border-l-4 border-green-400">
                 Modelo: Mikrotik Lhg Xl 5 Ac 802.11a/n/ac
