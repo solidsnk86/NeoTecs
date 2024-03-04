1ee08d94f505a403034c332397b7b1fc3e946cf6
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-03-04 20:39:50 -0300

    pririty next Img

--- src/components/ImageComponent.tsx
+++ src/components/ImageComponent.tsx
@@ -1,17 +1,31 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 interface ImageComponentProps {
     src: string;
     className?: string;
-    altImage?: string
+    altImage?: string;
+    priority?: boolean;
 }
 
-const ImageComponent: React.FC<ImageComponentProps> = ({ src, className, altImage }) => {
+const ImageComponent: React.FC<ImageComponentProps> = ({
+    src,
+    className,
+    altImage,
+    priority,
+}) => {
     const imageAlt = `${altImage}` + `,url de la imagen: ${src}`;
     return (
         <figure className="images-client">
-            <Image src={src} className={className} width={100} height={100} alt={imageAlt} layout="responsive" priority />
+            <Image
+                src={src}
+                className={className}
+                width={100}
+                height={100}
+                alt={imageAlt}
+                layout="responsive"
+                priority={priority}
+            />
         </figure>
     );
 };
