f2655c4d571e4554bd141191722797f56bfbfe45
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-03-06 10:14:54 -0300

    update

--- src/components/RamdomColor.js
+++ src/components/RamdomColor.js
@@ -1,5 +1,4 @@
-// ColorChangingComponent.js
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const ColorChangingComponent = () => {
   const [currentColor, setCurrentColor] = useState('#C084FC');
