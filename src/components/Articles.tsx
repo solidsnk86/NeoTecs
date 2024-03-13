dc72b3380b50c5a84f943834c3d71190d42925ce
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-03-13 13:48:23 -0300

    update

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,4 +1,4 @@
-import { HomeBlock } from '../pages/index'
+import { HomeBlock } from '../pages/index';
 import { githubAvatar } from './Constants';
 import LinkButton from './LinkButton';
 
@@ -11,21 +11,33 @@ export const Articles = ({
     title: string;
     content: string;
     src: string;
-    publishedAt: number,
+    publishedAt: number;
 }) => {
     return (
         <HomeBlock>
             <div className="h-[300px] relative text-left text-wrap space-y-2 dark:bg-[#09090B] bg-[#FFFFFF] p-4 rounded-lg border dark:border-zinc-800 shadow-sm shadow-slate-200 dark:!shadow z-50">
-                <span className='float-left text-xs'>Publicado el {publishedAt}</span><br />
-                <header className='flex'>
-                    <img src={githubAvatar} className='rounded-full w-[33px] h-[33px]' alt='Avatar profile' />
-                    <p className='mx-2 mt-[6px] text-text-second font-semibold text-sm'>Gabriel C.</p>
+                <span className="float-left text-xs">Publicado el {publishedAt}</span>
+                <br />
+                <header className="flex">
+                    <img
+                        src={githubAvatar}
+                        className="rounded-full w-[33px] h-[33px]"
+                        alt="Avatar profile"
+                    />
+                    <p className="mx-2 mt-[6px] text-text-second font-semibold text-sm">
+                        Gabriel C.
+                    </p>
                 </header>
-                <p className="text-xl font-bold ">
-                    {title}
-                </p>
+                <p className="text-xl font-bold ">{title}</p>
                 <p>{content}</p>
-                <LinkButton url={src} iconName='ArrowRight' color='red-400' className='xl:absolute bottom-4 right-4 block'>Ir al artículo</LinkButton>
+                <LinkButton
+                    url={src}
+                    iconName="ArrowRight"
+                    color="red-400"
+                    className="xl:absolute bottom-4 right-4 block"
+                >
+                    Ir al artículo
+                </LinkButton>
             </div>
         </HomeBlock>
     );
