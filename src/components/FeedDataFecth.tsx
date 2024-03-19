e0fdc27a7f16a52f1c5bd42543f604a7c09e0f5a
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-03-19 08:33:03 -0300

    update

--- src/components/FeedDataFecth.tsx
+++ src/components/FeedDataFecth.tsx
@@ -54,7 +54,7 @@ export const FeedbackData = () => {
     const sendMail = (item) => {
         const issues = 'https://github.com/solidsnk86/NeoTecs/issues/new'
         const subject = 'subjetc=Gracias por tu feedback'
-        const body = `body=Hola${item.nombre}! Es un agrado para mí recibir retroalimentación en mi web, estaré analizando en breve tu comentario, puedes generar un <a href=${issues}>issue</a> en Github para discutirlo. Desde ya muchas gracias! Gabriel de NeoTecs.-`
+        const body = `body=Hola ${item.nombre}! Es un agrado para mí recibir retroalimentación en mi web, estaré analizando en breve tu comentario, puedes generar un issue en ${issues}, para discutirlo. Desde ya muchas gracias! Gabriel de NeoTecs.-\n\n`
         const emailLink = `mailto:${item.email}?${subject}&${body}`;
         window.open(emailLink);
     };
@@ -63,7 +63,7 @@ export const FeedbackData = () => {
         <>
             {items.map((item) => (
                 <div
-                    className="bg-white dark:bg-zinc-800/50 border border-gray-300 dark:border-zinc-700 p-6 rounded-md shadow-md my-10 mx-auto xl:w-1/2"
+                    className="bg-white dark:bg-zinc-800/50 border border-gray-300 dark:border-zinc-700 p-6 rounded-md shadow-md my-10 mx-auto xl:w-1/2 relative z-50"
                     key={item.id}
                 >
                     <div className="text-gray-600 dark:text-gray-300 text-sm xl:text-base mb-4">
--- src/pages/docs/user-feedback.js
+++ src/pages/docs/user-feedback.js
@@ -1,41 +1,29 @@
 import { ArrowLeftIcon } from 'lucide-react';
-import { useState } from 'react';
 import { Footer } from '../../components/Footer';
 import { FeedbackData } from '../../components/FeedDataFecth';
 import { Nav } from '../../components/Nav';
 import { TitlesContextProvider } from '../../components/TitlesContextProvider';
 import HeaderTitle from '../../components/HeaderTitlte';
 import Indextitle from '../../components/IndexTitle';
-import LoginForm from './login';
 
-export default function UserFeedback({ user }) {
-  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
-
-  const handleCloseModal = () => {
-    setLoginModalOpen(false);
-  };
-
+export default function UserFeedback() {
   return (
     <TitlesContextProvider>
       <Nav className="fixed xl:relative w-full h-12 z-50" />
-      <div className="bg-[#F7F9F9] dark:bg-[#16181C] border-zinc-200/50 dark:border-zinc-800 border rounded p-1 xl:left-6 xl:top-14 top-14 left-2 fixed mt-3">
+      <div className="bg-[#F7F9F9] dark:bg-[#16181C] border-zinc-200/50 dark:border-zinc-800 border rounded p-1 xl:left-6 xl:top-14 top-14 left-2 fixed mt-3 hover:border-zinc-300 dark:hover:border-zinc-600 shadow-md dark:hover:brightness-125 cursor-pointer duration-300">
         <ArrowLeftIcon
-          className="text-text-primary cursor-pointer hover:translate-x-[-2px] transition-all"
+          className="text-text-primary"
           onClick={(e) => history.back(e)}
         />
       </div>
-      {user ? (
-        <div className="max-w-screen-xl mx-auto flex items-stretch py-16">
-          <div className="w-full max-w-none prose px-4 md:px-8 text-text-primary">
-            <HeaderTitle>Feedback de los usuarios</HeaderTitle>
-            <hr className="border-text-primary" />
-            <Indextitle>Comentarios:</Indextitle>
-            <FeedbackData />
-          </div>
+      <div className="max-w-screen-xl mx-auto flex items-stretch py-16">
+        <div className="w-full max-w-none prose px-4 md:px-8 text-text-primary">
+          <HeaderTitle>Feedback de los usuarios</HeaderTitle>
+          <hr className="border-text-primary" />
+          <Indextitle>Comentarios:</Indextitle>
+          <FeedbackData />
         </div>
-      ) : (
-        <LoginForm onClose={handleCloseModal} />
-      )}
+      </div>
       <Footer />
     </TitlesContextProvider>
   );
