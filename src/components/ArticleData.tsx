b1d1eaf6f48fe73a7bf2abeced50c94fdca3c5e3
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-04-14 03:01:20 -0300

    delete console log swiper

--- src/components/ArticleData.tsx
+++ src/components/ArticleData.tsx
@@ -6,61 +6,59 @@ import 'swiper/css';
 import { Preloader } from './Preloader';
 
 const ArticleData = () => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(articlesURL);
-            const csvData = await response.text();
-            const parsedData = csvData
-                .split('\n')
-                .slice(1)
-                .map((row) => {
-                    const [title, content, url, published] = row.split(',');
-                    return { title, content, url, published };
-                });
-            setData(parsedData);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(articlesURL);
+      const csvData = await response.text();
+      const parsedData = csvData
+        .split('\n')
+        .slice(1)
+        .map((row) => {
+          const [title, content, url, published] = row.split(',');
+          return { title, content, url, published };
+        });
+      setData(parsedData);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <div className="p-4">
-            {data.length === 0 ? (
-                <Preloader />
-            ) : (
-                <Swiper
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    pagination={{ clickable: true }}
-                    breakpoints={{
-                        640: {
-                            slidesPerView: 2,
-                        },
-                        1024: {
-                            slidesPerView: 3,
-                        }
-                    }}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
-                >
-                    {data.map((article, index) => (
-                        <SwiperSlide key={index}>
-                            <article className='p-4'>
-                                <Articles
-                                    title={article.title}
-                                    content={article.content}
-                                    src={article.url}
-                                    publishedAt={article.published}
-                                />
-                            </article>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            )}
-        </div>
-    );
+  return (
+    <div className="p-4">
+      {data.length === 0 ? (
+        <Preloader />
+      ) : (
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {data.map((article, index) => (
+            <SwiperSlide key={index}>
+              <article className="p-4">
+                <Articles
+                  title={article.title}
+                  content={article.content}
+                  src={article.url}
+                  publishedAt={article.published}
+                />
+              </article>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </div>
+  );
 };
 
 export default ArticleData;
