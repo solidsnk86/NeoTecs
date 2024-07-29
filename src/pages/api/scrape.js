0b01a5f9aa72225cc4a1a042c28e0d9eb0f92ee7
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-07-29 17:50:02 -0300

    update route middleware

--- src/pages/api/scrape.js
+++
@@ -1,8 +0,0 @@
-import { spawn } from 'child_process';
-
-export default async function handler(req, res) {
-  const process = spawn('python', ['/api.py']);
-  process.stdout.on('data', (data) => {
-    res.send(data.toString());
-  });
-}
