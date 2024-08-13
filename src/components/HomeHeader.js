0887d2f717c91fae802bc391758e70030175624d
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-12 21:17:42 -0300

    fix: margint top from home header

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export const HomeHeader = () => {
   return (
-    <div className="-mt-16">
+    <div className="-mt-22">
       <div className="max-w-screen-lg mx-auto pt-32 pb-16 px-4 md:px-8 lg:px-16 relative">
         <div className="background-light">
           <Atropos
