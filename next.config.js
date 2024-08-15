7b345335c768ccab4923558322c60a6eaf12ea84
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-15 10:19:01 -0300

    update pwa options

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
 });
 
 module.exports = withPWA({
