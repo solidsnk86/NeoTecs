1087a80dd25c80d08789196f2ada841d963c803e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-18 01:49:32 -0300

    fix _document

--- src/components/YouTubeVideo.js
+++ src/components/YouTubeVideo.js
@@ -1,12 +1,18 @@
 /* eslint-disable @next/next/no-sync-scripts */
 export function YouTubeVideo({ video, cpe, children }) {
   return (
-    <article className="pb-4 overflow-hidden">
-      <lite-youtube videoid={video}></lite-youtube>
-      <h3 className="text-lg text-text-primary text-left my-2 font-bold line-clamp-1 xl:line-clamp-2">
-        {cpe}
-      </h3>
-      <div className="text-sm text-left">{children}</div>
-    </article>
+    <>
+      <script
+        type="module"
+        src="https://cdn.jsdelivr.net/npm/@justinribeiro/lite-youtube@1.5.0/lite-youtube.js"
+      ></script>
+      <article className="pb-4 overflow-hidden">
+        <lite-youtube videoid={video}></lite-youtube>
+        <h3 className="text-lg text-text-primary text-left my-2 font-bold line-clamp-1 xl:line-clamp-2">
+          {cpe}
+        </h3>
+        <div className="text-sm text-left">{children}</div>
+      </article>
+    </>
   );
 }
