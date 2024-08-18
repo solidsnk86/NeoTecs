fbdbdc3885b82c53c1a26af4798fdc20b3d13015
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-18 01:22:47 -0300

    added script lite-youtube

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+//eslint-disable-next-line @next/next/no-sync-scripts
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -15,6 +16,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
+          <script
+            type="module"
+            src="https://cdn.jsdelivr.net/npm/@justinribeiro/lite-youtube@1.5.0/lite-youtube.js"
+          ></script>
         </Head>
         <body id="root">
           <Main />
