44eba051931b350df5bae8f009543a1d901f26bc
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-28 22:09:57 -0300

    remove button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Bug, Github, MessageCircleIcon, MoonStarIcon } from 'lucide-react';
+import { Bug, Github, MessageCircleIcon } from 'lucide-react';
 import { NeotecsLogo } from './NeotecsLogo';
 
 export const Nav = ({ className }) => {
@@ -13,21 +13,6 @@ export const Nav = ({ className }) => {
         </Link>
       </div>
       <aside className="flex space-x-3 mt-3">
-        <button className="flex items-center space-x-2 mb-3 hover:opacity-[.8] hover:transition-all">
-          <span
-            className="px-[3px] pt-[2px] hidden sm:block rounded-md border bg-card text-text-primary hover:bg-bg-card hover:border-zinc-400 dark:border-zinc-700 duration-100 font-semibold xl:text-sm text-xs"
-            title="Reporte de bichos"
-          >
-            <MoonStarIcon className="inline mb-1 cursor-pointer w-4 h-4 mx-1" />
-            Dark
-          </span>
-          <span
-            className="px-[3px] pt-[2px] sm:hidden rounded-md border bg-card text-text-primary dark:border-zinc-700 font-semibold xl:text-sm text-xs"
-            title="Reporte de bichos"
-          >
-            <Bug className="inline mb-1 cursor-pointer w-4 h-4 mx-1" />
-          </span>
-        </button>
         <Link
           href="https://github.com/solidsnk86/NeoTecs/issues/new"
           className="flex items-center space-x-2 mb-3 hover:opacity-[.8] hover:transition-all"
