a4b4d18992d9e0cb658a3f8e4c1ed44fb8e420ab
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-13 13:54:00 -0300

    remove arr dep

--- src/components/utils/tracker.js
+++ src/components/utils/tracker.js
@@ -56,7 +56,7 @@ export default function Tracker() {
     };
 
     fetchData();
-  }, [lastVisit, visitData]);
+  }, []);
 
   const getLastVisit = async () => {
     const response = await fetch('/api/visit-data');
