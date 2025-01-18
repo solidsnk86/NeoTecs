b30c0f7e57267cf6d9d61200566cef6ee0c17ddf
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-17 21:40:00 -0300

    improve styles in the svg badge

--- src/pages/api/badge-counter.js
+++ src/pages/api/badge-counter.js
@@ -45,6 +45,11 @@ export default async function badgerCount(req, res) {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${formatThousand(
       newCount,
     )} 30" width="${formatThousand(newCount)}" height="30">
+     <style>#badge-main:hover{opacity: 0.6;transition: .6s all;cursor:default;}
+     #text-counter:hover{fill:#4183c4;transition:.6s all;cursor:default;}
+     #eyes {animation: blink 4s infinite;}
+     @keyframes blink {0%,100% {opacity: 1;}95% {opacity: 1;}96% {opacity: 0.1;}98% {opacity: 0.1;}99% {opacity: 1;}}
+     </style>
     <!-- Fondo con gradiente -->
     <defs>
         <linearGradient id="bg-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
@@ -57,14 +62,15 @@ export default async function badgerCount(req, res) {
             }"/>
         </linearGradient>
     </defs>
-
-    <rect width="184" height="30" fill="url(#bg-gradient)"/>
+    
+    <rect id="badge-main" width="184" height="30" fill="url(#bg-gradient)"/>
 
     <rect x="142" width="42" height="30" fill="url(#count-gradient)"/>
 
-    <text y="19" x="6" font-size="16" text-rendering="geometricPrecision">👀</text>
-    <text x="33" y="20" fill="#fff" font-family="Arial, sans-serif" font-size="14" margin-right="4px" text-rendering="geometricPrecision" font-weight="700">Visitas al perfil</text>
-    <text x="${adjustCounter(
+    <text id="eyes" y="19" x="6" font-size="16" text-rendering="geometricPrecision">👀</text>
+    <text x="33" y="20" fill="#fff" font-family="Arial, sans-serif" font-size="14" margin-right="4px" text-rendering="geometricPrecision" 
+    font-weight="700">Visitas al perfil</text>
+    <text id="text-counter" x="${adjustCounter(
       newCount,
     )}" y="20" fill="#fff" font-family="Arial, sans-serif" font-size="${
       newCount >= 1000 ? '12' : '14'
