a052a96bab59c13ef5b9d63288e4c4208c38a2c1
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-20 13:22:22 -0300

    fix curly braces

--- src/components/Donations.tsx
+++ src/components/Donations.tsx
@@ -6,7 +6,7 @@ export const Donations = () => {
     <Link
       className={`w-fit border border-zinc-200/50 dark:border-zinc-800/50 p-3 my-16 rounded-[14px] flex justify-center mx-auto items-center 
         cursor-pointer gap-2 bg-zinc-100 dark:bg-zinc-900 relative shadow-md dark:hover:brightness-125 transition-all duration-300`}
-      href={'https://link.mercadopago.com.ar/neotecs'}
+      href="https://link.mercadopago.com.ar/neotecs"
     >
       <CoffeeIcon
         className="w-10 h-10 flex justify-center mx-auto py-1 px-2 border border-amber-300/50 rounded-xl bg-amber-200/30"
